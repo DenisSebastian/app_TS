2d87a68a7ef5f08727c736439bbada0e1966cc2d
DenisBerroeta <denis.berroeta@uai.cl>
2022-06-04 19:34:58 -0400

    corrección de label año

--- app.py
+++ app.py
@@ -28,12 +28,14 @@ newdf = df.loc[(df.serie == serie_filter)].dropna()
 width = 800
 height = 600
 
-fig = px.line(newdf, x='date', y='ndvi', title=title_plot, width = width, height= height)
+fig = px.line(newdf, x='date', y='ndvi', title=title_plot,
+        width = width, height= height)
+
 fig.update_xaxes(rangeslider_visible=True,
      rangeselector=dict(
         buttons=list([
             dict(count=6, label="6 meses", step="month", stepmode="backward"),
-            dict(count=1, label="1 año", step="year", stepmode="todate"),
+            dict(count=1, label="1 año", step="year", stepmode="backward"),
             dict(count=2, label="2 años", step="year", stepmode="backward"),
             dict(count=5, label="5 años", step="year", stepmode="backward"),
             dict(step="all")
