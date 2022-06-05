d62f03747a305291d43de64dd8f8011a05e7ffaa
DenisBerroeta <denis.berroeta@uai.cl>
2022-06-04 21:47:36 -0400

    Ordenar filtros en clumnas

--- app.py
+++ app.py
@@ -4,40 +4,48 @@ import os
 import plotly.express as px
 
 st.title("Series de NDVI")
-st.subheader("Visualización de Inicial datos de Prueba")
+st.subheader("Visualización inicial datos de prueba")
 
-# Selección de Bases
-base_select = st.selectbox(
-    label = "Seleccione Base",
-    options = ["disturbed", "non_disturbed"])
 
+col1, col2 = st.columns(2)
+
+with col1:
+    # Selección de Bases
+    base_select = st.selectbox(
+        label = "Seleccione Base",
+        options = ["disturbed", "non_disturbed"])
+
+# Generación path
 wd = os.getcwd()
 file_name = str(base_select + "_df.csv")
 files_csv = "data/csv"
 fullname = os.path.join(wd,files_csv , file_name)
 
 
-#st.write("""
-#Serie de tiempo de NDVI
-#""")
-
+# Lectura de Base
 df = pd.read_csv(fullname)
 #df = df.set_index("date")
-serie_filter = "f_000"
+
+with col2:
+    # Selector de Serie
+    serie_filter = st.selectbox("Seleccionar Serie", pd.unique(df["serie"]))
 
 
-serie_filter = st.selectbox("Seleccionar Serie", pd.unique(df["serie"]))
+
 title_plot = str("Time Series NDVI serie "+serie_filter)
 df_serie = df.loc[(df.serie == serie_filter)]
 
-checkbox_na = st.checkbox("Remove NA", value = True)
 
+
+# checkbox para remove NA values de la serie
+checkbox_na = st.checkbox("Remove NA", value = True)
 if checkbox_na:
     newdf = df_serie.dropna()
 else:
     newdf = df_serie
 
-width = 800
+# Generación de Visualización
+width = 750
 height = 550
 
 fig = px.line(newdf, x='date', y='ndvi', title=title_plot,
@@ -56,11 +64,14 @@ fig.update_xaxes(rangeslider_visible=True,
 )
 st.plotly_chart(fig, use_container_width=False)
 
-#st.line_chart(newdf.iloc[:, 2])
+# Descipción de la APP
+
 st.subheader("Descipción:")
 st.write("Corresponde a registros del promedio NDVI (Landsat 8) de parcelas bosque esclerofilo de la zona Central de Chile, por un periodo desde 2000-01-03 a 2022-04-29")
 
 st.latex(r'''
 NDVI = \frac{NIR-RED}{NIR+RED}
      ''')
+
+st.image("images/descp_ndvi.jpeg")
 st.markdown("**Autor:** [denis.berroeta@uai.cl](denis.berroeta@uai.cl)")
